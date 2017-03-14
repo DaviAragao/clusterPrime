d43c3b530bb789e11c41a928a95855a4b8eeb2d0
DaviAragao <davi.aragao@cpb.com.br>
2017-03-14 07:51:49 -0300

    Meu estilo de codificação

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char *argv[])
 {
 	int i, s, p, mp;
 
-	p = atoi(argv[1]); /* Lemos p da linha de comando */
+	p = atoi(argv[1]);
 
-	mp = 2; for ( i = 2 ; i <= p ; i++ ) mp *= 2; mp--;
-	/* Calculamos mp. */
+	mp = 2;
+	for(i = 2; i <= p; i++)
+		mp *= 2;
+	mp--;
 
 	s = 4;
-	for ( i = 2 ; i <= p - 1 ; i++ )
-	{ s = s*s - 2; printf("%d\n",s); }
-	/* Calculamos s(p-2). */
+	for(i = 2; i <= p - 1; i++)
+	{
+		s = s * s - 2;
+		printf("%d\n", s);
+	}
 
-	if ( s%mp == 0 ) /* Verificamos de s eh multiplo de mp. */
-	{ printf("M%d é primo.\n\n",p); }
+	if(s % mp == 0)
+		printf("M%d é primo.\n\n", p);
 	else
-	{ printf("M%d é composto.\n\n",p); }
+		printf("M%d é composto.\n\n", p);
 }

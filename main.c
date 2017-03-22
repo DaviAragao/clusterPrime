90807530f6f648f7c8812c2ae2b29b8768d90bfc
DaviAragao <davi.aragao@cpb.com.br>
2017-03-21 22:26:45 -0300

    Adicionando e usando a biblioteca GMP. 50%

--- main.c
+++ main.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
 
-unsigned long int calcMersenne(unsigned long int expoente);
+unsigned long int mersenne(unsigned long int expoente);
 unsigned long int lucasLehmer(unsigned long int expoente, unsigned long int mersenneNumber);
 
-int main()
+int main(int argc, char *argv[])
 {
-	unsigned long int p, s, mp;
+	unsigned long p, s, mp;
+	mpz_t pp, ll, aux;
+
+	mpz_init(pp);
+	mpz_init(ll);
+	mpz_init(aux);
 
 	for (p = 3; ; p += 2)
 	{
-		mp = calcMersenne(p);
+		mp = mersenne(p);
 		s = lucasLehmer(p, mp);
 		if (s == 0)
-			printf("M%lu é primo. - %lu -\n\n", p, mp);
+			printf("M%lu é primo. - %lu \n\n", p, mp);
 	}
+
+	mpz_set_ui(ll, (long)(4));
+
+	for (i = 1; i + 2 <= p; i++)
+	{
+		mpz_powm_ui(aux, ll, (long)(2), pp); 
+		mpz_sub_ui(ll, aux, (long)(2)); 
+	}
+
+	if (mpz_sgn(ll) == 0)
+		printf("M%ld é primo.\n",p);
+	else
+		printf("M%ld é composto.\n",p);
+
 }
 
-unsigned long int calcMersenne(unsigned long int expoente)
+mpz_t mersenne(unsigned long expoente)
 {
-	unsigned long int mp = 2, i;
-	for (i = 2; i <= expoente; i++)
-		mp *= 2;
-	return --mp;
+	mpz_t mersenneNumber;
+
+	mpz_init(mersenneNumber);
+	mpz_ui_pow_ui(mersenneNumber, (long)(2), expoente);
+	mpz_sub_ui(mersenneNumber, (long)(1));
+
+	return mersenneNumber;
 }
 
 unsigned long int lucasLehmer(unsigned long int expoente, unsigned long int mersenneNumber)

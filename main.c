d826beac6a57a8dc6dc36077a8e5ce23903236e1
DaviAragao <davi.aragao@cpb.com.br>
2017-03-22 20:38:49 -0300

    Implantação da biblioteca GMP - 100%

--- main.c
+++ main.c
@@ -1,57 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <gmp.h>
 
-unsigned long int mersenne(unsigned long int expoente);
-unsigned long int lucasLehmer(unsigned long int expoente, unsigned long int mersenneNumber);
-
-int main(int argc, char *argv[])
+int main()
 {
-	unsigned long p, s, mp;
-	mpz_t pp, ll, aux;
+	system("clear");
+	system("date");
+	int p, i;
+	mpz_t mersenneNumber, s, aux;
 
-	mpz_init(pp);
-	mpz_init(ll);
+	mpz_init(mersenneNumber);
+	mpz_init(s);
 	mpz_init(aux);
 
 	for (p = 3; ; p += 2)
 	{
-		mp = mersenne(p);
-		s = lucasLehmer(p, mp);
-		if (s == 0)
-			printf("M%lu é primo. - %lu \n\n", p, mp);
+		mpz_ui_pow_ui(mersenneNumber, (long)(2), p);
+		mpz_sub_ui(mersenneNumber, mersenneNumber, (long)(1));
+		mpz_set_ui(s, (long)(4));
+
+		for (i = 1; i <= p - 2; i++)
+		{
+			mpz_powm_ui(aux, s, (long)(2), mersenneNumber); 
+			mpz_sub_ui(s, aux, (long)(2)); 
+		}
+
+		if (mpz_sgn(s) == 0)
+		{
+			gmp_printf("M%d%c %Zd %c%c", p, 10, mersenneNumber, 10, 10);
+			system("date");
+		}
 	}
-
-	mpz_set_ui(ll, (long)(4));
-
-	for (i = 1; i + 2 <= p; i++)
-	{
-		mpz_powm_ui(aux, ll, (long)(2), pp); 
-		mpz_sub_ui(ll, aux, (long)(2)); 
-	}
-
-	if (mpz_sgn(ll) == 0)
-		printf("M%ld é primo.\n",p);
-	else
-		printf("M%ld é composto.\n",p);
-
-}
-
-mpz_t mersenne(unsigned long expoente)
-{
-	mpz_t mersenneNumber;
-
-	mpz_init(mersenneNumber);
-	mpz_ui_pow_ui(mersenneNumber, (long)(2), expoente);
-	mpz_sub_ui(mersenneNumber, (long)(1));
-
-	return mersenneNumber;
-}
-
-unsigned long int lucasLehmer(unsigned long int expoente, unsigned long int mersenneNumber)
-{
-	unsigned long int s = 4, i;
-	for (i = 1; i <= expoente - 2; i++)
-		s = (s * s - 2) % mersenneNumber;
-	return s;
 }

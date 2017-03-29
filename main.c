56aec82648c6d363e396b5292d33d36a492bfd87
DaviAragao <davi.aragao@cpb.com.br>
2017-03-28 22:18:00 -0300

    MultiThreading 50%

--- main.c
+++ main.c
@@ -1,32 +1,36 @@
 #include <gmp.h>
+#include <pthread.h>
 
+void *getPrime(void *p);
 int main()
 {
-	system("clear");
-	system("date");
-	int p, i;
+	int p;
+	pthread_t thread;
+
+	for (p = 3; ; p += 2)
+		if (!pthread_create(&thread, NULL, getPrime, &p))
+		gmp_printf("Thread criada. Expoente %d%c%c", p, 10);
+}
+
+void *getPrime(void *p)
+{
 	mpz_t mersenneNumber, s, aux;
+	int i, expoente = * (int *) p;
 
 	mpz_init(mersenneNumber);
 	mpz_init(s);
 	mpz_init(aux);
 
-	for (p = 3; ; p += 2)
+	mpz_ui_pow_ui(mersenneNumber, (long)(2), expoente);
+	mpz_sub_ui(mersenneNumber, mersenneNumber, (long)(1));
+	mpz_set_ui(s, (long)(4));
+
+	for (i = 1; i <= expoente - 2; i++)
 	{
-		mpz_ui_pow_ui(mersenneNumber, (long)(2), p);
-		mpz_sub_ui(mersenneNumber, mersenneNumber, (long)(1));
-		mpz_set_ui(s, (long)(4));
-
-		for (i = 1; i <= p - 2; i++)
-		{
-			mpz_powm_ui(aux, s, (long)(2), mersenneNumber); 
-			mpz_sub_ui(s, aux, (long)(2)); 
-		}
-
-		if (mpz_sgn(s) == 0)
-		{
-			gmp_printf("M%d%c %Zd %c%c", p, 10, mersenneNumber, 10, 10);
-			system("date");
-		}
+		mpz_powm_ui(aux, s, (long)(2), mersenneNumber); 
+		mpz_sub_ui(s, aux, (long)(2)); 
 	}
+
+	if (mpz_sgn(s) == 0)
+		gmp_printf("M%d%c%Zd %c%c", expoente, 10, mersenneNumber, 10, 10);
 }

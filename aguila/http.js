e3b7ecccb35482843ea152e437dc9c15ad44b8b3
DaviAragao <davi.aragao@cpb.com.br>
2017-04-19 20:39:37 -0300

    Testes com NodeJs

---
+++ aguila/http.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+	request.on("end", function () {
+		response.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+		response.end('Hello HTTP!');
+	});
+}).listen(8080);
